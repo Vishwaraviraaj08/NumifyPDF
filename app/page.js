7e155cd11f403e2b22208f1f145039988c6d4620
shiva1718 <ms.shiva2019@gmail.com>
2023-12-30 20:23:48 +0530

    Add hyperlinks to authors' names in footer
    The authors' names in the footer of the page have been updated to include hyperlinks to their Github profiles. This allows users to navigate directly to the authors' profiles, enhancing the visibility and accessibility of their other work.

--- app/page.js
+++ app/page.js
@@ -123,7 +123,7 @@ Add Page Numbers
                     </div>
                 </div>
                 <footer className="p-6 mt-5 bg-blue-900 text-center text-white">
-                    <p className="mb-2">By Vishwa Raviraaj and Shiva Sankar</p>
+                    <p className="mb-2">By <a className={"underline"} href={"https://github.com/Vishwaraviraaj08/"}>Vishwa Raviraaj</a> and <a className={"underline"} href={"https://github.com/shiva1718"}>Shiva Sankar</a></p>
                     <a href="https://github.com/Vishwaraviraaj08/NumifyPDF" className="underline text-white"
                        target="_blank">Please Star me on GitHub🤩</a>
                 </footer>
